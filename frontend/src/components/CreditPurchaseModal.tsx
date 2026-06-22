fc1b123bcbfa274d4edb13461c066b41dfd1829f
Akbar Khowaja <akbar.khowaja@gmail.com>
2026-06-22 00:14:06 -0500

    fix(phase3): correct CreditPurchaseModal schema import

--- frontend/src/components/CreditPurchaseModal.tsx
+++ frontend/src/components/CreditPurchaseModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useAuth } from '../contexts/AuthContext'
-import { CreditPurchaseSchema } from '../lib/schemas'
+import { LetterCreditPurchaseSchema } from '../lib/schemas'
 
 interface CreditPurchaseModalProps {
   nonprofitEin: string
